efec83e48d686413ff23be31b32f9d39599f582e
Erik Lantto <lantto@gmail.com>
2014-07-12 01:08:03 +0200

    Initial commit

---
+++ ptth.js
@@ -0,0 +1,58 @@
+(function() {
+
+var module = angular.module('ptth', []);
+
+module.provider('ptth', function() {
+    var baseUrl     = '', 
+        dataMethods = ['post', 'put', 'patch'],
+        _$http;
+
+    this.setBaseUrl = function(url) {
+        baseUrl = url;
+    }
+
+    function prependUrl(url) {
+        return baseUrl + url;
+    }
+
+    function createShortMethods(http, methods) {
+        for (var i = 0; i < methods.length; i++) {
+            (function (method) {
+                http[method] = function() {
+                    var config, 
+                        url = arguments[0];
+
+                    if (dataMethods.indexOf(method) > -1) {
+                        config = arguments[2] || {};
+                        config.data = arguments[1];
+                    } else {
+                        config = arguments[1] || {};
+                    }
+
+                    config.method = method;
+                    config.url = prependUrl(url);
+
+                    return _$http(config);
+                }
+            })(methods[i]);
+        };
+    }
+
+    this.$get = ['$http', function($http) {
+        _$http = $http;
+
+        var http = function (config) {
+            config.url = prependUrl(config.url);
+
+            return $http(config);
+        };
+
+        createShortMethods(http, ['get', 'delete', 'head', 'jsonp', 'post', 'put', 'patch']);
+
+        http.pendingRequests = $http.pendingRequests;
+
+        return http;
+    }];
+});
+
+})();
